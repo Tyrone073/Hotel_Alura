28dd803bed0af44b79627e4ce3075882d0cd76a7
Tyrone Pilay <tyronepilay11@gmail.com>
2023-04-19 14:41:37 -0500

    agregando imagenes

--- src/main/java/com/tyrone/hotel_alura/view/HotelAluraFrame.java
+++ src/main/java/com/tyrone/hotel_alura/view/HotelAluraFrame.java
@@ -1,5 +1,27 @@
 package com.tyrone.hotel_alura.view;
 
-public class HotelAluraFrame {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class HotelAluraFrame extends JFrame {
+//	public static void main(String[] args) {
+//		JFrame frame = new JFrame("Ejemplo de imagen");
+//		
+//		// Crea una nueva instancia de ImageIcon a partir de la ruta de la imagen
+//		ImageIcon imagen = new ImageIcon("com/tyrone/hotel_alura/IMAGENES/menu-img.png");
+////		ImageIcon imagen = new ImageIcon("C:/Users/59399/eclipse-workspace/hotel_alura/src/main/resources/IMAGENES/menu-img.png");
+//		
+//		// Crea un nuevo objeto JLabel con la imagen
+//		JLabel label = new JLabel(imagen);
+//		
+//		// Agrega el JLabel al JFrame
+//		frame.add(label);
+//		
+//		// Configura las dimensiones y la visibilidad del JFrame
+//		frame.setSize(600, 600);
+//		frame.setVisible(true);
+//		
+//	}
 
 }
