a887483a52f217eef930595563327e3f43d90818
Tyrone Pilay <tyronepilay11@gmail.com>
2023-05-14 16:24:04 -0500

    mejorando la interfaz de usuario

--- src/main/java/com/tyrone/hotel_alura/view/HotelAluraFrame.java
+++ src/main/java/com/tyrone/hotel_alura/view/HotelAluraFrame.java
@@ -5,11 +5,15 @@ import java.awt.*;
 
 public class HotelAluraFrame extends JFrame {
 	private  JFrame frame ;
-	private JPanel panel;
+	private JPanel panel = new JPanel(new BorderLayout()); ;//se crea un panel con la posibilidad de utilizar el borde q es para decirle una direccion
+
+
 
 	private JLabel labelIzquierda;
 
-	private JLabel labelSuperiorDerecha;
+	private JLabel labelImagenDerecha;
+
+	JPanel panelDerecho = new JPanel(new GridLayout(3,0));
 	private JPanel footer;
 	//public static void main(String[] args) {
 	public HotelAluraFrame(){
@@ -17,7 +21,6 @@ public class HotelAluraFrame extends JFrame {
 		frame = new JFrame("Ejemplo de imagen");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		panel = new JPanel(new BorderLayout());
 		panel.setBackground(Color.blue);
 
 		// Panel izquierdo (imagen)
@@ -27,11 +30,25 @@ public class HotelAluraFrame extends JFrame {
 		panel.add(labelIzquierda, BorderLayout.WEST);//la agregamos en el panel y debe de recibir un dato de tipo BorderLayout aprobechamos para decirl en q parte de lo coloca
 
 		// Panel superior derecho
-		JPanel panelSuperiorDerecho = new JPanel(new GridLayout(2, 1));
-		ImageIcon imagenSuperiorDerecha = new ImageIcon(HotelAluraFrame.class.getResource("/imagenes/login.png"));
-		labelSuperiorDerecha = new JLabel(imagenSuperiorDerecha);
-		panelSuperiorDerecho.add(labelSuperiorDerecha);
-		panel.add(panelSuperiorDerecho, BorderLayout.CENTER);
+		//JPanel panelSuperiorDerecho = new JPanel(new GridLayout(2, 1));
+		ImageIcon imagenSuperiorDerecha = new ImageIcon(HotelAluraFrame.class.getResource("/imagenes/aH-150px.png"));
+		labelImagenDerecha = new JLabel(imagenSuperiorDerecha);
+		panelDerecho.add(labelImagenDerecha);
+		//panel.add(panelDerecho, BorderLayout.EAST);// este es el posisionamiento del panel derecho y creo q deve de ser casteado una sola vez
+
+		//JPanel panelCentralDerecho = new JPanel(new GridLayout(3, 1));//creo un panel q tiene 3 filas
+		ImageIcon imagenCentralDerecha = new ImageIcon(HotelAluraFrame.class.getResource("/imagenes/login.png"));//instatancio una imagen
+		labelImagenDerecha = new JLabel(imagenCentralDerecha);//combierto la imagen en un objeto label
+		panelDerecho.add(labelImagenDerecha);//agrego ese objeto a panel creado de 3 fila
+		//panel.add(panelCentralDerecho, BorderLayout.CENTER);// agrego se objeto panel al panel padre
+		//panel.add(panelDerecho, BorderLayout.EAST);
+
+		ImageIcon imagenInferiorDerecha = new ImageIcon(HotelAluraFrame.class.getResource("/imagenes/cerrar-sesion 32-px.png"));//instatancio una imagen
+		labelImagenDerecha = new JLabel(imagenInferiorDerecha);//combierto la imagen en un objeto label
+		panelDerecho.add(labelImagenDerecha);//agrego este objeto imagen al panelderecho
+
+
+		panel.add(panelDerecho, BorderLayout.EAST);// este guarda los demas paneles derechos 
 
 		// Panel inferior derecho
 		 footer = new JPanel(new FlowLayout(FlowLayout.CENTER));//para poner el texto en el centro
